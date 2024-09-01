073e6e5a727bfea0c88d810f123b858942b05807
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-09-02 00:55:40 +0500

    linkedin scrapper

---
+++ backend/platform_scrapper/scrappers/linkedin_scrapper.py
@@ -0,0 +1,18 @@
+from linkedin_api import Linkedin
+
+
+class LinkedinScrapper:
+    def __init__(self, email, password):
+        self.api = Linkedin(email, password)
+
+    def get_profile(self, username):
+        return self.api.get_profile(username)
+
+    def get_profile_posts(self, username, max_results=10):
+        return self.api.get_profile_updates(username, post_count=max_results)
+    
+    def get_profile_updates(self, username, max_results=10):
+        return self.api.get_profile_updates(username, max_results=max_results)
+
+
+        
