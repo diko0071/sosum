14cba9a65a342b7d52b8e3db33c9965f31b29481
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-09-04 01:51:00 +0500

    additonal method of verification in linkedin

--- backend/platform_scrapper/scrappers/linkedin_scrapper.py
+++ backend/platform_scrapper/scrappers/linkedin_scrapper.py
@@ -1,9 +1,18 @@
 from linkedin_api import Linkedin
-
+from requests.cookies import cookiejar_from_dict
+import os
 
 class LinkedinScrapper:
-    def __init__(self, email, password):
-        self.api = Linkedin(email, password)
+    def __init__(self, email, password, method='credentials'):
+        if method == 'credentials':
+            self.api = Linkedin(email, password)
+        elif method == 'cookie':
+            cookies = cookiejar_from_dict({
+                'liap': 'true',
+                'JSESSIONID': os.getenv('LINKEDIN_COOKIE_JSESSIONID'),
+                'liat': os.getenv('LINKEDIN_COOKIE_LI_AT'),
+            })
+            self.api = Linkedin('', '', cookies=cookies)
 
     def get_profile(self, username):
         return self.api.get_profile(username)
