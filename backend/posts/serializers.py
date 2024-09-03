97590ee68e8eb6ce83419ca2bcf5f7efd0feabcf
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-09-03 21:04:27 +0500

    duplication validation

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -9,13 +9,25 @@ class PostContentSerializer(serializers.ModelSerializer):
         fields = (
             'title', 'description', 'post_source_url', 'post_source_id', 'author', 'ai_summary', 'platform', 'tags', 'scrapper_log_id', 'post_source_date'
         )
-
+    
+    def create(self, validated_data):
+        existing_post = PostContent.objects.filter(post_source_id=validated_data['post_source_id']).first()
+        if existing_post:
+            return existing_post
+        return super().create(validated_data)
+    
 class PostSocialSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostSocial
         fields = (
-            'post_id', 'post_source_url', 'post_source_id', 'platform', 'tags', 'scrapper_log_id', 'post_source_date', 'author'
+            'post_source_url', 'post_source_id', 'platform', 'tags', 'scrapper_log_id', 'post_source_date', 'author'
         )
+
+    def create(self, validated_data):
+        existing_post = PostSocial.objects.filter(post_source_id=validated_data['post_source_id']).first()
+        if existing_post:
+            return existing_post
+        return super().create(validated_data)
 
 class PromptLogSerializer(serializers.ModelSerializer):
     class Meta:
